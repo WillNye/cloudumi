7b3d2c6d45a8eb580142b2d72f07cb2270dc4a5f
Curtis Castrapel <ccastrapel@gmail.com>
2021-12-12 14:51:10 -0800

    Elasticache support

--- cloudumi_terraform/saas_central_account/elasticache.tf
+++ cloudumi_terraform/saas_central_account/elasticache.tf
@@ -1,3 +1,67 @@
+#####
+# VPC and subnets
+#####
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+#####
+# Elasticache Redis
+#####
+module "redis" {
+  source  = "umotif-public/elasticache-redis/aws"
+  version = "2.2.0"
+
+  name_prefix           = "redis-clustered-example"
+  number_cache_clusters = 2
+  node_type             = "cache.t3.small"
+
+  cluster_mode_enabled    = true
+  replicas_per_node_group = 1
+  num_node_groups         = 1
+
+  engine_version           = "6.x"
+  port                     = 6379
+  maintenance_window       = "mon:03:00-mon:04:00"
+  snapshot_window          = "04:00-06:00"
+  snapshot_retention_limit = 7
+
+  automatic_failover_enabled = true
+
+  at_rest_encryption_enabled = true
+  transit_encryption_enabled = true
+  auth_token                 = "1234567890asdfghjkl"
+
+  apply_immediately = true
+  family            = "redis6.x"
+  description       = "Test elasticache redis."
+
+  subnet_ids = data.aws_subnet_ids.all.ids
+  vpc_id     = data.aws_vpc.default.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  parameter = [
+    {
+      name  = "repl-backlog-size"
+      value = "16384"
+    }
+  ]
+
+  tags = {
+    Project = "Test"
+  }
+}
+
+
+####
+
+# Legacy
+
 // module "redis-cluster" {
 //     source                      = "clouddrove/elasticache/aws"
 //     version                     = "0.15.0"
@@ -20,24 +84,24 @@
 //   }
 
 
-resource "aws_elasticache_cluster" "cloudumi-customer-cache" {
-  cluster_id           = "cloudumi-es"
-  replication_group_id = aws_elasticache_replication_group.cloudumi-customer-cache.id
-}
+// resource "aws_elasticache_cluster" "cloudumi-customer-cache" {
+//   cluster_id           = "cloudumi-es"
+//   replication_group_id = aws_elasticache_replication_group.cloudumi-customer-cache.id
+// }
 
-resource "aws_elasticache_replication_group" "cloudumi-customer-cache" {
-  replication_group_id          = "cloudumi-redis-cluster"
-  replication_group_description = "Cloudumi Redis Cluster Replication Group"
-  node_type                     = "cache.t3.micro"
-  transit_encryption_enabled    = false
-  at_rest_encryption_enabled    = true
-  port                          = 6379
-  parameter_group_name          = "default.redis6.x.cluster.on"
-  automatic_failover_enabled    = true
+// resource "aws_elasticache_replication_group" "cloudumi-customer-cache" {
+//   replication_group_id          = "cloudumi-redis-cluster"
+//   replication_group_description = "Cloudumi Redis Cluster Replication Group"
+//   node_type                     = "cache.t3.micro"
+//   transit_encryption_enabled    = false
+//   at_rest_encryption_enabled    = true
+//   port                          = 6379
+//   parameter_group_name          = "default.redis6.x.cluster.on"
+//   automatic_failover_enabled    = true
 
-  cluster_mode {
-    replicas_per_node_group = 0
-    num_node_groups         = 1
-  }
-  snapshot_retention_limit = 1
-}
+//   cluster_mode {
+//     replicas_per_node_group = 0
+//     num_node_groups         = 1
+//   }
+//   snapshot_retention_limit = 1
+// }
