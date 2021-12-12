dc3d979b237035c7238bc446c8615110302b4dd6
Curtis Castrapel <ccastrapel@gmail.com>
2021-12-12 09:53:42 -0800

    Don't load ptvsd unless DEBUG_ENABLED is set

--- util/debug/__init__.py
+++ util/debug/__init__.py
@@ -1,17 +1,17 @@
 import os
-import ptvsd
-
-# To debug over network, set corresponding env var to 0.0.0.0
-DEBUG_HOST = os.getenv('DEBUG_HOST', 'localhost')
-
-# Set the debug port to attach to
-DEBUG_PORT = os.getenv('DEBUG_PORT', 9092)
 
 # Set this to true in the environment if debugging is desired
-DEBUG_ENABLED = os.getenv('DEBUG', False)
-
+DEBUG_ENABLED = os.getenv("DEBUG", False)
 if DEBUG_ENABLED:
+    import ptvsd
+
+    # To debug over network, set corresponding env var to 0.0.0.0
+    DEBUG_HOST = os.getenv("DEBUG_HOST", "localhost")
+
+    # Set the debug port to attach to
+    DEBUG_PORT = os.getenv("DEBUG_PORT", 9092)
+
     print(" ===> DEBUG ENABLED THERE GOOD BUDDYS <=== ")
-    ptvsd.enable_attach(address=('localhost', DEBUG_PORT), redirect_output=True)
+    ptvsd.enable_attach(address=("localhost", DEBUG_PORT), redirect_output=True)
     print("Debugger listening on port 9092. Waiting for debugger to attach...")
     ptvsd.wait_for_attach()
