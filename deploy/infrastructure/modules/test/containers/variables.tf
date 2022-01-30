b8e6890ae75b7b404063333054d5d95aff4596a5
Matt Daue <mattdaue@gmail.com>
2022-01-30 11:24:58 -0800

    Add Output
    Signed-off-by: Matt Daue <mattdaue@gmail.com>

--- deploy/infrastructure/modules/test/containers/variables.tf
+++ deploy/infrastructure/modules/test/containers/variables.tf
@@ -7,3 +7,13 @@ variable "ecs_task_execution_role" {
   description = "The task IAM execution role"
   type = string
 }
+
+variable "region" {
+  description = "The region set as per the output"
+  type = string
+}
+
+variable "stage" {
+  description = "The stage set as per the output"
+  type = string
+}
