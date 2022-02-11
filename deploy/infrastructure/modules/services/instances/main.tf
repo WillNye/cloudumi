70b9cb0199e5f909d7caff470d88f762574748c8
Curtis Castrapel <curtis@noq.dev>
2022-02-11 15:45:05 +0000

    Configure SSM on Ubuntu

--- deploy/infrastructure/modules/services/instances/main.tf
+++ deploy/infrastructure/modules/services/instances/main.tf
@@ -49,7 +49,7 @@ resource "aws_instance" "jumpbox" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.jumpbox_sg.id]
   subnet_id                   = var.public_subnet_ids[0]
-
+  iam_instance_profile = "${aws_iam_instance_profile.jumpbox_instance_profile.id}"
   credit_specification {
     cpu_credits = "unlimited"
   }
@@ -62,4 +62,43 @@ resource "aws_instance" "jumpbox" {
     http_tokens   = "required"
     http_endpoint = "enabled"
   }
+}
+
+#Instance Role
+resource "aws_iam_role" "jumpbox_role" {
+  name = "${var.cluster_id}-jumpboxInstanceRole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+#Instance Profile
+resource "aws_iam_instance_profile" "jumpbox_instance_profile" {
+  name = "${var.cluster_id}-jumpboxInstanceRole"
+  role = "${aws_iam_role.jumpbox_role.id}"
+}
+
+#Attach Policies to Instance Role
+resource "aws_iam_policy_attachment" "jumpbox_ssm_policy_1" {
+  name       = "${var.cluster_id}-ssm-attachment_1"
+  roles      = [aws_iam_role.jumpbox_role.id]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_policy_attachment" "jumpbox_ssm_policy_2" {
+  name       = "${var.cluster_id}-ssm-attachment_2"
+  roles      = [aws_iam_role.jumpbox_role.id]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
