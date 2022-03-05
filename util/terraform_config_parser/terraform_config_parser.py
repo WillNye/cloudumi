3f5bcbc0fb700ebbc8a9086c7e956f3ae9eff0c1
Curtis Castrapel <ccastrapel@gmail.com>
2022-03-04 22:05:53 -0800

    WIP

--- util/terraform_config_parser/terraform_config_parser.py
+++ util/terraform_config_parser/terraform_config_parser.py
@@ -284,6 +284,12 @@ if __name__ == "__main__":
         config_output_path,
     )
     write_file(
+        "ecs_deployer.py.jinja2",
+        "ecs_deployer.py",
+        terraform_config,
+        config_output_path,
+    )
+    write_file(
         "compose.yaml.jinja2", "compose.yaml", terraform_config, config_output_path
     )
     write_file("ecs.yaml.jinja2", "ecs.yaml", terraform_config, config_output_path)
