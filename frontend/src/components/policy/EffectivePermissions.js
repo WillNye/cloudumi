581e2deecc71e74ff2472a755b73ad0bf85b89e0
Curtis Castrapel <ccastrapel@gmail.com>
2022-03-10 08:04:19 -0800

    Add linting messages

--- frontend/src/components/policy/EffectivePermissions.js
+++ frontend/src/components/policy/EffectivePermissions.js
@@ -1,55 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import { Header, Segment } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Header, Segment, Icon, Message } from 'semantic-ui-react'
 import MonacoDiffComponent from '../blocks/MonacoDiffComponent'
-import { useAuth } from '../../auth/AuthProviderDefault'
 import useEffectivePermissions from './hooks/useEffectivePermissions'
 
 const EffectivePermissions = () => {
-  // TODO: Put actual account names here
-  // const { get } = useApi('services/aws/policies/effective/role/759357822767/development_admin')
-  // const { error, success } = useToast()
-
-  // useEffect(() => get.do(), [])
-
-  // const handleRefresh = () => get.do()
   const [error, setError] = useState(null)
   const [messages, setMessages] = useState([])
   const { resourceEffectivePermissions } = useEffectivePermissions()
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const resJson = await sendRequestCommon(
-  //       null,
-  //       '/api/v3/services/aws/policies/effective/role/759357822767/development_admin',
-  //       'get'
-  //     )
-  //     if (!resJson) {
-  //       return
-  //     }
-  //     setData(resJson.data)
-  //   }
-  //   fetchData()
-  // }, [sendRequestCommon])
   const onLintError = (lintErrors) => {
     if (lintErrors.length > 0) {
       setError(true)
-      setMessages(lintErrors)
+      setMessages(JSON.stringify(lintErrors))
     } else {
       setError(false)
       setMessages([])
     }
   }
 
+  // TODO: Make it possible to request a change that removes all unused permissions
+  // TODO: Give the user commands to do this manually
   return (
     <>
       <Header as='h2'>
         Effective Permissions
         <Header.Subheader>
-          The effective permissions of a role is a combination of the
+          The effective permissions of a role are a combination of the
           permissions contained in a role's inline policies and managed
           policies. The effective permissions are de-duplicated, minimized, and
-          alphabetically sorted. This is shown side-by-side with the role's
-          effective policy with all unused permissions removed.
+          alphabetically sorted. The right side shows the effective permissions
+          of a role with all permissions removed if they have not been used in
+          the last 90 days.
         </Header.Subheader>
       </Header>
       <Segment
@@ -60,25 +41,34 @@ const EffectivePermissions = () => {
         }}
       >
         {resourceEffectivePermissions ? (
-          <MonacoDiffComponent
-            onLintError={onLintError}
-            oldValue={JSON.stringify(
-              resourceEffectivePermissions.effective_policy,
-              null,
-              2
-            )}
-            newValue={JSON.stringify(
-              resourceEffectivePermissions.effective_policy_unused_permissions_removed,
-              null,
-              2
-            )}
-            // readOnly={
-            //   (!config.can_update_cancel && !config.can_approve_reject) ||
-            //   changeReadOnly
-            // }
-            //  onLintError={onLintError}
-            //  onValueChange={onValueChange}
-          />
+          <>
+            {error ? (
+              <Message warning attached='top'>
+                <Icon name='warning' />
+                {messages}
+              </Message>
+            ) : null}
+
+            <MonacoDiffComponent
+              onLintError={onLintError}
+              oldValue={JSON.stringify(
+                resourceEffectivePermissions.effective_policy,
+                null,
+                2
+              )}
+              newValue={JSON.stringify(
+                resourceEffectivePermissions.effective_policy_unused_permissions_removed,
+                null,
+                2
+              )}
+              // readOnly={
+              //   (!config.can_update_cancel && !config.can_approve_reject) ||
+              //   changeReadOnly
+              // }
+              //  onLintError={onLintError}
+              //  onValueChange={onValueChange}
+            />
+          </>
         ) : null}
       </Segment>
       {/* <JustificationModal handleSubmit={handleAssumeRolePolicySubmit} /> */}
