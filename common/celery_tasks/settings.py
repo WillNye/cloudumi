e34fef63932fc0217e17bab611d936938a2cdaf0
Matt Daue <matt@noq.dev>
2022-03-16 12:58:59 -0700

    Add debug troubleshooting instructions
    Signed-off-by: Matt Daue <matt@noq.dev>

--- common/celery_tasks/settings.py
+++ common/celery_tasks/settings.py
@@ -1,7 +1,9 @@
+import boto3
+from common.config import config
 from common.celery_tasks import app
-from common.lib.assume_role import boto3_cached_conn
 
 
 @app.task
 def synchronize_sso():
-    client = boto3_cached_conn("cognito-idp")
+    client = boto3.client("cognito-idp", region_name=config.region)
+    
