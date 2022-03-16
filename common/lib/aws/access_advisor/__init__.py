0b31bad97a66cd4f6c399eac007df797bbe253ad
Curtis Castrapel <ccastrapel@gmail.com>
2022-03-16 12:02:45 -0700

    Moar docstrings

--- common/lib/aws/access_advisor/__init__.py
+++ common/lib/aws/access_advisor/__init__.py
@@ -1,7 +1,7 @@
 import copy
 import datetime
 import time
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
 from blinker import Signal
 
@@ -66,7 +66,9 @@ class AccessAdvisor:
             5 * 60
         )  # Wait 5 minutes before giving up on jobs
 
-    async def store_access_advisor_results(self, account_id, host, access_advisor_data):
+    async def store_access_advisor_results(
+        self, account_id: str, host: str, access_advisor_data: Dict[str, Any]
+    ) -> bool:
         """Stores Access Advisor results in S3 for identities across an account.
 
         :param account_id: AWS Account ID
@@ -86,6 +88,7 @@ class AccessAdvisor:
             ).format(account_id=account_id),
             host=host,
         )
+        return True
 
     async def generate_and_save_access_advisor_data(self, host: str, account_id: str):
         """Generates effective and removed permissions
@@ -115,7 +118,7 @@ class AccessAdvisor:
         account_id: str,
         arns: List[str],
         access_advisor_data: Dict[str, Any],
-    ):
+    ) -> bool:
         """Generates and saves the "effective permissions" for each identity
         arn in `arns`.
 
@@ -144,9 +147,10 @@ class AccessAdvisor:
             ).format(account_id=account_id),
             host=host,
         )
+        return True
 
     @rate_limited()
-    def _generate_service_last_accessed_details(self, iam, arn):
+    def _generate_service_last_accessed_details(self, iam, arn: str) -> str:
         """Generates the last accessed time for each service for a given identity.
         Wraps it around rate limit protection.
 
@@ -157,8 +161,21 @@ class AccessAdvisor:
         return iam.generate_service_last_accessed_details(Arn=arn)["JobId"]
 
     @rate_limited()
-    def _get_service_last_accessed_details(self, iam, job_id, marker=None):
-        """Wrapping the actual AWS API calls for rate limiting protection."""
+    def _get_service_last_accessed_details(
+        self, iam, job_id: str, marker: Optional[str] = None
+    ) -> Dict[str, Any]:
+        """Retrieves service last access details job status. Wraps it around
+        rate limit protection.
+
+        :param iam: Boto3 IAM client
+        :param job_id: Job ID from `generate_service_last_accessed_details` call
+        :param marker: Pagination marker
+        :return: get_service_last_accessed_details results, IE:
+            {'JobStatus': 'COMPLETED', 'JobType': 'SERVICE_LEVEL',
+            'JobCreationDate': datetime.datetime(20...o=tzutc()),
+            'ServicesLastAccessed': [{...}, {...}, {...}, {...}, {...}, {...}, {...}, {...}, {...}],
+            'JobCompletionDate': datetime.datetime(20...o=tzutc()), 'IsTruncated': False,...}}
+        """
         params = {
             "JobId": job_id,
         }
@@ -166,7 +183,14 @@ class AccessAdvisor:
             params["Marker"] = marker
         return iam.get_service_last_accessed_details(**params)
 
-    def _generate_job_ids(self, iam, arns):
+    def _generate_job_ids(self, iam, arns: List[str]) -> Dict[str, str]:
+        """Calls generate_service_last_accessed_details for each identity, and
+        generates a list of each pending Job ID
+
+        :param iam: Boto3 IAM client
+        :param arns: A list of identity ARNs
+        :return: A dictionary of `job_id` to `arn`
+        """
         jobs = {}
         for role_arn in arns:
             try:
@@ -187,7 +211,13 @@ class AccessAdvisor:
                 )
         return jobs
 
-    def _get_job_results(self, iam, jobs):
+    def _get_job_results(self, iam, jobs: Dict[str, str]) -> List[Dict[str, Any]]:
+        """Gets the results of the job IDs in `jobs`.
+
+        :param iam: Boto3 IAM client
+        :param jobs: A dictionary of `job_id` to `arn`
+        :return: Cumulative Access Advisor data for identities across a single AWS account
+        """
         access_details = {}
         job_queue = list(jobs.keys())
         last_job_completion_time = time.time()
@@ -269,6 +299,11 @@ class AccessAdvisor:
         return access_details
 
     def _log_unfinished_jobs(self, job_queue, job_details):
+        """Logs the jobs that are unfinished.
+
+        :param job_queue: _description_
+        :param job_details: _description_
+        """
         for job_id in job_queue:
             role_arn = job_details[job_id]
             log.error(
