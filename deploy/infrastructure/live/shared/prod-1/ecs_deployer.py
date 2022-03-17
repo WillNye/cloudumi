d6790dfe23bc5748287e720d5e2c966ca272804d
Curtis Castrapel <ccastrapel@gmail.com>
2022-03-17 09:31:00 -0700

    Remove old files

--- deploy/infrastructure/live/shared/prod-1/ecs_deployer.py
+++ deploy/infrastructure/live/shared/prod-1/ecs_deployer.py
@@ -8,9 +8,37 @@ from botocore.exceptions import ClientError
 
 current_path = pathlib.Path(__file__).parent.resolve()
 
-task_definition_yaml_f = f"{current_path}/task_definition.yaml"
+service_task_definition_map = [
+    {
+        "service": "api",
+        "task_definition": f"{current_path}/task_definition_api.yaml",
+        "desiredCount": 1,
+        "loadBalancers": [
+            {
+                "containerName": "noq-dev-shared-prod-1-api",
+                "containerPort": 8092,
+                "targetGroupArn": "arn:aws:elasticloadbalancing:us-west-2:940552945933:targetgroup/tf-20220210004707474900000002/bff21530b0388c25",
+            },
+        ],
+    },
+    {
+        "service": "celery_scheduler",
+        "task_definition": f"{current_path}/task_definition_celery_scheduler.yaml",
+        "desiredCount": 1,
+    },
+    {
+        "service": "celery_worker",
+        "task_definition": f"{current_path}/task_definition_celery_worker.yaml",
+        "desiredCount": 1,
+    },
+    {
+        "service": "celery_flower",
+        "task_definition": f"{current_path}/task_definition_celery_flower.yaml",
+        "desiredCount": 1,
+    },
+]
+
 cluster_name = "noq-dev-shared-prod-1"
-service_name = "noq-dev-shared-prod-1"
 subnets = ["subnet-0335e107c814d63f5", "subnet-06b4ff38d90fa1b9b"]
 security_groups = ["sg-0e7a1ca3c697feb53"]
 os.environ["AWS_PROFILE"] = "noq_prod"
@@ -20,9 +48,6 @@ kms_key_arn = (
     "arn:aws:kms:us-west-2:940552945933:key/4705da2e-1c2a-4594-bf31-b240e1daa8ab"
 )
 noq_ecs_log_group_name = "noq-dev-shared-prod-1"
-
-with open(task_definition_yaml_f, "r") as f:
-    task_definition = yaml.load(f, Loader=yaml.FullLoader)
 
 ecr_client = boto3.client("ecr", region_name=region)
 response = ecr_client.get_authorization_token(
@@ -67,78 +92,101 @@ except ClientError as e:
         },
     )
 
-registered_task_definition = ecs_client.register_task_definition(**task_definition)
+for service in service_task_definition_map:
+    service_name = service["service"]
 
-task_definition_name = "{}:{}".format(
-    registered_task_definition["taskDefinition"]["family"],
-    registered_task_definition["taskDefinition"]["revision"],
-)
+    with open(service["task_definition"], "r") as f:
+        task_definition = yaml.load(f, Loader=yaml.FullLoader)
 
-try:
-    service = ecs_client.create_service(
-        cluster=cluster_name,
-        serviceName=service_name,
-        taskDefinition=task_definition_name,
-        desiredCount=1,
-        launchType="FARGATE",
-        enableExecuteCommand=True,
-        networkConfiguration={
-            "awsvpcConfiguration": {
-                "subnets": subnets,
-                "assignPublicIp": "DISABLED",
-                "securityGroups": security_groups,
-            }
-        },
-    )
-except ClientError as e:
-    if e.response["Error"] != {
-        "Message": "Creation of service was not idempotent.",
-        "Code": "InvalidParameterException",
-    }:
-        raise
-    service = ecs_client.update_service(
-        cluster=cluster_name,
-        service=service_name,
-        taskDefinition=task_definition_name,
-        desiredCount=1,
-        enableExecuteCommand=True,
-        networkConfiguration={
-            "awsvpcConfiguration": {
-                "subnets": subnets,
-                "assignPublicIp": "DISABLED",
-                "securityGroups": security_groups,
-            }
-        },
-    )
+        registered_task_definition = ecs_client.register_task_definition(
+            **task_definition
+        )
+
+        task_definition_name = "{}:{}".format(
+            registered_task_definition["taskDefinition"]["family"],
+            registered_task_definition["taskDefinition"]["revision"],
+        )
+
+        try:
+            service = ecs_client.create_service(
+                cluster=cluster_name,
+                serviceName=service_name,
+                taskDefinition=task_definition_name,
+                desiredCount=service["desiredCount"],
+                launchType="FARGATE",
+                loadBalancers=service.get("loadBalancers", []),
+                enableExecuteCommand=True,
+                networkConfiguration={
+                    "awsvpcConfiguration": {
+                        "subnets": subnets,
+                        "assignPublicIp": "DISABLED",
+                        "securityGroups": security_groups,
+                    }
+                },
+            )
+        except ClientError as e:
+            if e.response["Error"] != {
+                "Message": "Creation of service was not idempotent.",
+                "Code": "InvalidParameterException",
+            }:
+                raise
+            service = ecs_client.update_service(
+                cluster=cluster_name,
+                service=service_name,
+                taskDefinition=task_definition_name,
+                desiredCount=1,
+                loadBalancers=service.get("loadBalancers", []),
+                enableExecuteCommand=True,
+                networkConfiguration={
+                    "awsvpcConfiguration": {
+                        "subnets": subnets,
+                        "assignPublicIp": "DISABLED",
+                        "securityGroups": security_groups,
+                    }
+                },
+            )
+
+service_rollout_completed = 0
+rollout_finalized = False
 
 while True:
-    service_status = ecs_client.describe_services(
-        cluster=cluster_name, services=[service_name]
-    )
-    rollout_finalized = False
-    for service in service_status["services"]:
-        for deployment in service["deployments"]:
-            if deployment["status"] == "PRIMARY":
-                print(
-                    "Service: {}, Pending Count: {}, Running Count: {}, Rollout State: {}".format(
-                        service_name,
-                        deployment["pendingCount"],
-                        deployment["runningCount"],
-                        deployment["rolloutState"],
-                    )
-                )
-                if deployment["rolloutState"] == "COMPLETED":
-                    print("Rollout completed")
-                    rollout_finalized = True
-                    break
-                elif deployment["rolloutState"] == "FAILED":
-                    print("Rollout failed")
-                    rollout_finalized = True
-                    break
-                if deployment["failedTasks"] > 0:
-                    print("Rollout failed. Number of failed tasks is greater than 0")
-                    rollout_finalized = True
-                    break
-    if rollout_finalized:
+    if service_rollout_completed == len(service_task_definition_map):
         break
-    time.sleep(5)
+
+    for service in service_task_definition_map:
+        if service.get("status") in ["COMPLETED", "FAILED"]:
+            continue
+        service_name = service["service"]
+        service_status = ecs_client.describe_services(
+            cluster=cluster_name, services=[service_name]
+        )
+        for service_status in service_status["services"]:
+            for deployment in service_status["deployments"]:
+                if deployment["status"] == "PRIMARY":
+                    print(
+                        "Service: {}, Pending Count: {}, Running Count: {}, Rollout State: {}".format(
+                            service_name,
+                            deployment["pendingCount"],
+                            deployment["runningCount"],
+                            deployment["rolloutState"],
+                        )
+                    )
+                    if deployment["rolloutState"] == "COMPLETED":
+                        print(f"Service: {service_name}, Rollout completed")
+                        service["status"] = "COMPLETED"
+                        service_rollout_completed += 1
+                        break
+                    elif deployment["rolloutState"] == "FAILED":
+                        print(f"Service: {service_name}, Rollout failed")
+                        service["status"] = "FAILED"
+                        service_rollout_completed += 1
+                        break
+                    if deployment["failedTasks"] > 0:
+                        print(
+                            "Rollout failed. Number of failed tasks is greater than 0"
+                        )
+                        rollout_finalized = True
+                        break
+        if rollout_finalized:
+            break
+        time.sleep(5)
