326c81b177e0077cdc8f34fcb3b90cf24047dad9
Matt Daue <matt@noq.dev>
2022-03-17 13:16:25 -0700

    Fix failing unit tests
    Signed-off-by: Matt Daue <matt@noq.dev>

--- common/config/tests/test_role_access.py
+++ common/config/tests/test_role_access.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+import pytest
 import yaml
 from asgiref.sync import async_to_sync
 from tornado.httpclient import AsyncHTTPClient
@@ -31,6 +32,9 @@ def delete_role_access():
     )
 
 
+@pytest.mark.usefixtures("aws_credentials")
+@pytest.mark.usefixtures("dynamodb")
+@pytest.mark.usefixtures("with_test_configuration_tenant_static_config_data")
 class TestRoleAccess(TestCase):
     """Docstring in public class."""
 
