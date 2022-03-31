d36f532de03435397b39b1484d3ed71caa8036d4
Curtis Castrapel <ccastrapel@gmail.com>
2022-03-31 08:34:00 -0700

    Remove autodiscover_tasks call from run.py

--- common/celery_tasks/run.py
+++ common/celery_tasks/run.py
@@ -10,12 +10,6 @@ if __name__ == "__main__":
     logger.info("Starting up celery tasks")
     log_level = os.getenv("CELERY_LOG_LEVEL", "DEBUG")
     concurrency = os.getenv("CELERY_CONCURRENCY", "16") or "16"
-    celery_tasks.app.autodiscover_tasks(
-        [
-            "common.celery_tasks.auth",
-            "common.celery_tasks.settings",
-        ]
-    )
     celery_tasks.app.worker_main(
         [
             "worker",
