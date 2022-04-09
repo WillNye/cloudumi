2dc89c7ba41142a67804df397bf26046ae7e394d
Curtis Castrapel <ccastrapel@gmail.com>
2022-04-08 21:32:15 -0700

    SAAS-431: Customizable region for Cognito; Asyncio

--- api/handlers/v3/auth/sso/__init__.py
+++ api/handlers/v3/auth/sso/__init__.py
@@ -24,6 +24,7 @@ class IdpConfigurationCrudHandler(ConfigurationCrudHandler):
     _user_pool_client_id = None
     _model_class = None
     _sso_idp_attr = None
+    _user_pool_region = None
 
     @property
     def user_pool_id(self) -> str:
@@ -40,6 +41,20 @@ class IdpConfigurationCrudHandler(ConfigurationCrudHandler):
         return user_pool_id
 
     @property
+    def user_pool_region(self) -> str:
+        if user_pool_region := self._user_pool_region:
+            return user_pool_region
+
+        user_pool_region = config.get_host_specific_key(
+            "secrets.cognito.config.user_pool_region", self.ctx.host, config.region
+        )
+        if not user_pool_region:
+            raise ValueError("Cognito user pool id not configured")
+
+        self._user_pool_region = user_pool_region
+        return user_pool_region
+
+    @property
     def user_pool_client_id(self) -> str:
         if user_pool_client_id := self._user_pool_client_id:
             return user_pool_client_id
@@ -53,11 +68,13 @@ class IdpConfigurationCrudHandler(ConfigurationCrudHandler):
         self._user_pool_client_id = user_pool_client_id
         return user_pool_client_id
 
-    def _retrieve(self) -> dict:
-        cognito_idp = boto3.client("cognito-idp", region_name=config.region)
+    async def _retrieve(self) -> dict:
+        cognito_idp = boto3.client("cognito-idp", region_name=self.user_pool_region)
 
         try:
-            sso_idp_provider = identity.get_identity_providers(self.user_pool_id)
+            sso_idp_provider = await identity.get_identity_providers(
+                self.user_pool_id, client=cognito_idp
+            )
         except cognito_idp.exceptions.ResourceNotFoundException:
             raise ValueError
 
@@ -70,6 +87,7 @@ class IdpConfigurationCrudHandler(ConfigurationCrudHandler):
         return sso_idp_provider.dict()
 
     async def _create(self, data):
+        cognito_idp = boto3.client("cognito-idp", region_name=self.user_pool_region)
         if not self._sso_idp_attr:  # Upsert all the things
             sso_idp_provider = self._model_class(**data)
             sso_idp_provider.provider_name = sso_idp_provider.provider_type
@@ -77,15 +95,20 @@ class IdpConfigurationCrudHandler(ConfigurationCrudHandler):
             sso_idp_provider = SSOIDPProviders()
             setattr(sso_idp_provider, self._sso_idp_attr, self._model_class(**data))
 
-        return identity.upsert_identity_provider(
-            self.user_pool_id, self.user_pool_client_id, sso_idp_provider
+        return await identity.upsert_identity_provider(
+            self.user_pool_id,
+            self.user_pool_client_id,
+            sso_idp_provider,
+            client=cognito_idp,
         )
 
     async def _delete(self) -> bool:
-        cognito_idp = boto3.client("cognito-idp", region_name=config.region)
+        cognito_idp = boto3.client("cognito-idp", region_name=self.user_pool_region)
 
         try:
-            sso_idp_provider = identity.get_identity_providers(self.user_pool_id)
+            sso_idp_provider = await identity.get_identity_providers(
+                self.user_pool_id, client=cognito_idp
+            )
         except cognito_idp.exceptions.ResourceNotFoundException:
             raise ValueError
 
@@ -93,21 +116,29 @@ class IdpConfigurationCrudHandler(ConfigurationCrudHandler):
             self._sso_idp_attr
         ):  # If a specific identity provider was specified only delete that one
             provider = getattr(sso_idp_provider, self._sso_idp_attr, None)
-            identity.disconnect_idp_from_app_client(
-                self.user_pool_id, self.user_pool_client_id, provider
+            await identity.disconnect_idp_from_app_client(
+                self.user_pool_id,
+                self.user_pool_client_id,
+                provider,
+                client=cognito_idp,
             )
-            return identity.delete_identity_provider(self.user_pool_id, provider)
+            return await identity.delete_identity_provider(
+                self.user_pool_id, provider, client=cognito_idp
+            )
         else:  # Delete all supported providers
             supported_providers = list(SSOIDPProviders.__dict__["__fields__"].keys())
             deleted = True  # Have an all or nothing id on deleted so we don't exit on first bad delete
             for provider_type in supported_providers:
                 # If a request is being made to set an already defined provider, remove the existing provider
                 if provider := getattr(sso_idp_provider, provider_type):
-                    identity.disconnect_idp_from_app_client(
-                        self.user_pool_id, self.user_pool_client_id, provider
+                    await identity.disconnect_idp_from_app_client(
+                        self.user_pool_id,
+                        self.user_pool_client_id,
+                        provider,
+                        client=cognito_idp,
                     )
-                    if not identity.delete_identity_provider(
-                        self.user_pool_id, provider
+                    if not await identity.delete_identity_provider(
+                        self.user_pool_id, provider, client=cognito_idp
                     ):
                         deleted = False
 
@@ -156,9 +187,12 @@ class CognitoUserCrudHandler(CognitoCrudHandler):
     _model_class = CognitoUser
     _identifying_keys = ["Username"]
 
-    def _retrieve(self) -> list[dict]:
+    async def _retrieve(self) -> list[dict]:
+        cognito_idp = boto3.client("cognito-idp", region_name=self.user_pool_region)
         users = list()
-        for user in identity.get_identity_users(self.user_pool_id):
+        for user in await identity.get_identity_users(
+            self.user_pool_id, client=cognito_idp
+        ):
             user_dict: dict = user.dict()
             user_dict.pop("TemporaryPassword")
             for attr in user_dict.get("Attributes", []):
@@ -171,27 +205,30 @@ class CognitoUserCrudHandler(CognitoCrudHandler):
         return users
 
     async def _create(self, data) -> CognitoUser:
-        cognito_idp = boto3.client("cognito-idp", region_name=config.region)
+        cognito_idp = boto3.client("cognito-idp", region_name=self.user_pool_region)
 
         try:
             # Update the resource
             cognito_user = self._model_class(**data)
             cognito_user.Groups = [
                 x
-                for x in identity.get_identity_user_groups(
-                    self.user_pool_id, cognito_user
+                for x in await identity.get_identity_user_groups(
+                    self.user_pool_id, cognito_user, client=cognito_idp
                 )
             ]
-            identity.upsert_identity_user_group(self.user_pool_id, cognito_user)
+            await identity.upsert_identity_user_group(
+                self.user_pool_id, cognito_user, client=cognito_idp
+            )
         except cognito_idp.exceptions.UserNotFoundException:
             # Resource doesn't exist, so create it
-            return identity.create_identity_user(
-                self.user_pool_id, self._model_class(**data)
+            return await identity.create_identity_user(
+                self.user_pool_id, self._model_class(**data), client=cognito_idp
             )
 
     async def _delete(self, data) -> bool:
-        return identity.delete_identity_user(
-            self.user_pool_id, self._model_class(**data)
+        cognito_idp = boto3.client("cognito-idp", region_name=self.user_pool_region)
+        return await identity.delete_identity_user(
+            self.user_pool_id, self._model_class(**data), client=cognito_idp
         )
 
 
@@ -199,31 +236,38 @@ class CognitoGroupCrudHandler(CognitoCrudHandler):
     _model_class = CognitoGroup
     _identifying_keys = ["GroupName"]
 
-    def _retrieve(self) -> list[dict]:
+    async def _retrieve(self) -> list[dict]:
+        cognito_idp = boto3.client("cognito-idp", region_name=self.user_pool_region)
         return [
-            group.dict() for group in identity.get_identity_groups(self.user_pool_id)
+            group.dict()
+            for group in await identity.get_identity_groups(
+                self.user_pool_id, client=cognito_idp
+            )
         ]
 
     async def _create(self, data) -> CognitoGroup:
-        cognito_idp = boto3.client("cognito-idp", region_name=config.region)
+        cognito_idp = boto3.client("cognito-idp", region_name=self.user_pool_region)
         try:
             # Update the resource
-            cognito_group = identity.get_identity_group(
-                self.user_pool_id, data["GroupName"]
+            cognito_group = await identity.get_identity_group(
+                self.user_pool_id, data["GroupName"], client=cognito_idp
             )
             if (
                 description := data.get("Description")
             ) and description != cognito_group.Description:
                 cognito_group.Description = description
-                return identity.update_identity_group(self.user_pool_id, cognito_group)
+                return await identity.update_identity_group(
+                    self.user_pool_id, cognito_group, client=cognito_idp
+                )
 
         except cognito_idp.exceptions.ResourceNotFoundException:
             # Resource doesn't exist, so create it
-            return identity.create_identity_group(
-                self.user_pool_id, self._model_class(**data)
+            return await identity.create_identity_group(
+                self.user_pool_id, self._model_class(**data), client=cognito_idp
             )
 
     async def _delete(self, data) -> bool:
-        return identity.delete_identity_group(
-            self.user_pool_id, self._model_class(**data)
+        cognito_idp = boto3.client("cognito-idp", region_name=self.user_pool_region)
+        return await identity.delete_identity_group(
+            self.user_pool_id, self._model_class(**data), client=cognito_idp
         )
