c1e97b3c1de5fd8a5f2f06a8e76777892e9a3114
Curtis Castrapel <ccastrapel@gmail.com>
2022-04-12 14:18:27 -0700

    WIP

--- api/handlers/v3/automatic_policy_request_handler/aws.py
+++ api/handlers/v3/automatic_policy_request_handler/aws.py
@@ -1,9 +1,65 @@
-import tornado.escape
+import asyncio
+import random
+import string
 
+import tornado.escape
+import ujson as json
+from asgiref.sync import sync_to_async
+
+from common.config import config
+from common.config.models import ModelAdapter
 from common.handlers.base import BaseHandler
+from common.lib.assume_role import boto3_cached_conn
+from common.lib.aws.sanitize import sanitize_session_name
+from common.models import SpokeAccount
 
 
 class AutomaticPolicyRequestHandler(BaseHandler):
     async def post(self):
+        host = self.ctx.host
         data = tornado.escape.json_decode(self.request.body)
-        print(data)
+        role_arn = data.get("role")
+        if not role_arn:
+            raise Exception("Role ARN not defined")
+        # TODO: If role_arn, check to see if role_arn is flagged as in_development, and if self.user is authorized for this role
+        # TODO: Log all requests and actions taken during the session. eg: Google analytics for IAM
+        account_id = role_arn.split(":")[4]
+        principal_name = role_arn.split("/")[-1]
+        # TODO: Support draft policies, we keep updating the draft with new changes until the user wants to submit it? Not as "aha!"
+        # TODO: Normalize the policy, make sure the identity doesn't already have the allowance, and send the request. In our case, make the change.
+        spoke_role_name = (
+            ModelAdapter(SpokeAccount)
+            .load_config("spoke_accounts", host)
+            .with_query({"account_id": account_id})
+            .first.name
+        )
+        if not spoke_role_name:
+            return
+        iam_client = boto3_cached_conn(
+            "iam",
+            host,
+            account_number=account_id,
+            assume_role=spoke_role_name,
+            region=config.region,
+            sts_client_kwargs=dict(
+                region_name=config.region,
+                endpoint_url=f"https://sts.{config.region}.amazonaws.com",
+            ),
+            client_kwargs=config.get_host_specific_key("boto3.client_kwargs", host, {}),
+            session_name=sanitize_session_name("noq_automatic_policy_request_handler"),
+        )
+        letters = string.ascii_lowercase
+        policy_name = "".join(random.choice(letters) for i in range(10))
+        # TODO: Need to ask the policy the question if it already can do what is in the permission
+        # TODO: Generate formal permission request / audit trail
+        # TODO: Generate more meaningful policy name
+        # TODO: Generate cross-account resources as well
+        await sync_to_async(iam_client.put_role_policy)(
+            RoleName=principal_name,
+            PolicyName=policy_name,
+            PolicyDocument=json.dumps(
+                data["policy"],
+                escape_forward_slashes=False,
+            ),
+        )
+        await asyncio.sleep(5)
--- api/handlers/v3/tenant_registration/tenant_registration.py
+++ api/handlers/v3/tenant_registration/tenant_registration.py
@@ -543,6 +543,7 @@ class TenantRegistrationHandler(TornadoRequestHandler):
                 break
             dev_domain = await generate_dev_domain(dev_mode)
 
+        dev_domain = "unusual_noq_dev"
         # User pool domain names cannot have underscores
         user_pool_domain_name = dev_domain.replace("_", "-")
 
@@ -639,6 +640,7 @@ secrets:
         user_pool_id: {user_pool_id}
         user_pool_client_id: {cognito_client_id}
         user_pool_client_secret: {cognito_user_pool_client_secret}
+        user_pool_region: {config.region}
 get_user_by_oidc_settings:
   client_scopes:
     - email
@@ -647,7 +649,7 @@ get_user_by_oidc_settings:
   metadata_url: https://cognito-idp.{region}.amazonaws.com/{user_pool_id}/.well-known/openid-configuration
   jwt_verify: true
   jwt_email_key: email
-  jwt_groups_key: "cognito:groups"
+  jwt_groups_key: 'cognito:groups'
   grant_type: authorization_code
   id_token_response_key: id_token
   access_token_response_key: access_token
