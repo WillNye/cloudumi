39c2cc35b744c340551d05cab05724cf0bd0118e
Will <will@noq.dev>
2022-04-19 15:25:23 -0500

    Refactor of the AutomaticPolicyRequestHandler to start incorporating new auto policy request functions.

--- api/handlers/v3/automatic_policy_request_handler/aws.py
+++ api/handlers/v3/automatic_policy_request_handler/aws.py
@@ -1,12 +1,7 @@
 import tornado.escape
-from asgiref.sync import sync_to_async
 
-from common.config import config
-from common.config.models import ModelAdapter
 from common.handlers.base import BaseHandler
-from common.lib.assume_role import boto3_cached_conn
-from common.lib.aws.sanitize import sanitize_session_name
-from common.models import SpokeAccount
+from common.lib.policies import automatic_request
 
 
 class AutomaticPolicyRequestHandler(BaseHandler):
@@ -16,40 +11,7 @@ class AutomaticPolicyRequestHandler(BaseHandler):
         role_arn = data.get("role")
         if not role_arn:
             raise Exception("Role ARN not defined")
-        # TODO: If role_arn, check to see if role_arn is flagged as in_development, and if self.user is authorized for this role
-        # TODO: Log all requests and actions taken during the session. eg: Google analytics for IAM
-        account_id = role_arn.split(":")[4]
-        principal_name = role_arn.split("/")[-1]
-        # TODO: Normalize the policy, make sure the identity doesn't already have the allowance, and send the request. In our case, make the change.
-        spoke_role_name = (
-            ModelAdapter(SpokeAccount)
-            .load_config("spoke_accounts", host)
-            .with_query({"account_id": account_id})
-            .first.name
-        )
-        if not spoke_role_name:
-            return
-        iam_client = boto3_cached_conn(
-            "iam",
-            host,
-            account_number=account_id,
-            assume_role=spoke_role_name,
-            region=config.region,
-            sts_client_kwargs=dict(
-                region_name=config.region,
-                endpoint_url=f"https://sts.{config.region}.amazonaws.com",
-            ),
-            client_kwargs=config.get_host_specific_key("boto3.client_kwargs", host, {}),
-            session_name=sanitize_session_name("noq_automatic_policy_request_handler"),
-        )
-        policy_name = "generated_policy"
-        # TODO: Need to ask the policy the question if it already can do what is in the permission
-        # TODO: Generate formal permission request / audit trail
-        # TODO: Don't overwrite existing policies
-        # TODO: Generate cross-account resource policies as well, turn into a formal policy request
-        await sync_to_async(iam_client.put_role_policy)(
-            RoleName=principal_name,
-            PolicyName=policy_name,
-            PolicyDocument=data["policy"],
-        )
-        self.write({"applied": True})
+
+        applied = await automatic_request.create_policy(host, role_arn, data["policy"])
+
+        self.write({"applied": applied})
