e9883cce820d9c7ade1c9778de8c872800b8db5b
Matt Daue <matt@noq.dev>
2022-04-22 13:07:21 -0700

    Update populate_services
    Signed-off-by: Matt Daue <matt@noq.dev>

--- deploy/local/populate_services.py
+++ deploy/local/populate_services.py
@@ -8,14 +8,22 @@ os.environ.setdefault(
     "CONFIG_LOCATION", "configs/development_account/saas_development.yaml"
 )
 os.environ.setdefault("AWS_PROFILE", "noq_cluster_dev")
+override_email = os.getenv("OVERRIDE_EMAIL", "user@noq.dev")
 
 import common.scripts.initialize_dynamodb  # noqa: F401, E402
 from common.lib.dynamo import RestrictedDynamoHandler  # noqa: F401, E402
 
 tenant_config = f"""
-_development_user_override: user@noq.dev
+_development_user_override: {override_email}
 _development_groups_override:
   - engineering@noq.dev
+  - {override_email}
+notifications:
+  enabled: true
+cloudtrail:
+  enabled: true
+  account_id: "759357822767"
+  queue_arn: arn:aws:sqs:us-west-2:759357822767:noq-cloudtrail-access-denies
 cache_self_service_typeahead:
   cache_resource_templates: true
 cache_resource_templates:
@@ -71,22 +79,24 @@ spoke_accounts:
     restrict_viewers_of_account_resources: false
 org_accounts:
   - org_id: test_org
-    account_id: '123456789012'
+    account_id: '759357822767'
     account_name: test_account
     owner: user
 tenant_details:
   external_id: 018e23e8-9b41-4d66-85f2-3d60cb2b3c43
-  creator: user@noq.dev
+  creator: {override_email}
   creation_time: {int(time.time())}
 site_config:
   landing_url: /
+  notifications: enabled
+  request_interval: 1
 headers:
   identity:
     enabled: false
   role_login:
     enabled: true
 url: https://localhost
-application_admin: user@noq.dev
+application_admin: engineering@noq.dev
 secrets:
   jwt_secret: {token_urlsafe(32)}
   auth:
@@ -101,6 +111,9 @@ secrets:
       user_pool_client_secret: 1l4g523pb7rb3iicm9jod80nlst3r92f4oitg2dijna45pegj4dh
 account_ids_to_name:
   "759357822767": "development"
+celery:
+  cache_cloudtrail_denies:
+    enabled: true
 """
 
 # Store tenant information in DynamoDB
@@ -108,15 +121,22 @@ account_ids_to_name:
 ddb = RestrictedDynamoHandler()
 
 async_to_sync(ddb.update_static_config_for_host)(
-    tenant_config, "user@noq.dev", "localhost"
+    tenant_config, override_email, "localhost"
 )
 
 cloudumi_config = f"""
-_development_user_override: user@noq.dev
+_development_user_override: {override_email}
 _development_groups_override:
   - engineering@noq.dev
+  - {override_email}
 cache_self_service_typeahead:
   cache_resource_templates: true
+notifications:
+  enabled: true
+cloudtrail:
+  enabled: true
+  account_id: "759357822767"
+  queue_arn: arn:aws:sqs:us-west-2:759357822767:noq-cloudtrail-access-denies
 cache_resource_templates:
   repositories:
     - type: git
@@ -171,22 +191,24 @@ spoke_accounts:
     restrict_viewers_of_account_resources: false
 org_accounts:
   - org_id: test_org
-    account_id: 123456789012
+    account_id: '759357822767'
     account_name: test_account
     owner: user
 tenant_details:
   external_id: 018e23e8-9b41-4d66-85f2-3d60cb2b3c43
-  creator: user@noq.dev
+  creator: {override_email}
   creation_time: {int(time.time())}
 site_config:
   landing_url: /
+  notifications: enabled
+  request_interval: 1
 headers:
   identity:
     enabled: false
   role_login:
     enabled: true
 url: https://cloudumidev.com
-application_admin: user@noq.dev
+application_admin: engineering@noq.dev
 secrets:
   jwt_secret: {token_urlsafe(32)}
   auth:
@@ -220,6 +242,9 @@ get_user_by_oidc_settings:
   access_token_audience: null
 aws:
   automatically_update_role_trust_policies: false
+celery:
+  cache_cloudtrail_denies:
+    enabled: true
 """
 
 # Store cloudumidev information in DynamoDB
@@ -227,7 +252,7 @@ aws:
 ddb = RestrictedDynamoHandler()
 
 async_to_sync(ddb.update_static_config_for_host)(
-    cloudumi_config, "user@noq.dev", "cloudumidev_com"
+    cloudumi_config, override_email, "cloudumidev_com"
 )
 
 # Force a re-cache of cloud resources with updated configuration
