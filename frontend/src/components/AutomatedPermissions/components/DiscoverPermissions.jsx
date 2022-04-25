a8e1f43e90765e568bbb687d1ab3b64eb4b0beaf
Curtis Castrapel <ccastrapel@gmail.com>
2022-04-25 15:23:52 -0700

    Verbiage change

--- frontend/src/components/AutomatedPermissions/components/DiscoverPermissions.jsx
+++ frontend/src/components/AutomatedPermissions/components/DiscoverPermissions.jsx
@@ -8,26 +8,21 @@ const DiscoverPermissions = () => {
         <Dimmer active inverted>
           <Loader size='massive'>
             <p className='loader-text'>
-              <b> Listening for access denied error</b>
+              <b> Listening for Access Denied errors</b>
             </p>
           </Loader>
         </Dimmer>
       </Segment>
       <Segment className='description-text' basic>
         <p>
-          When AWS API operations are denied, Noq can propose policy changes to
-          grant access.
+          Configure Weep to run in proxy mode to automatically detect, generate,
+          and request policies to resolve Access Denied errors.
         </p>
-
         <p>
-          A Weep proxy searches for a policy by retrying those specific
-          operations with additional permissions, within the risk tolerance of
-          an Organization (see $link).
-        </p>
-
-        <p>
-          A proposal to add all of those permissions can be displayed for
-          review, submitted as a role request, or applied automatically.
+          If the requested permissions are within your organization's risk
+          tolerance, they will be automatically approved and applied to the
+          role, and Weep will transparently retry the request without ever
+          return an access denied error.
         </p>
       </Segment>
     </>
