53cb6e3e6a1c906122351c221a8c30c28f52c423
Will <will@noq.dev>
2022-06-07 11:48:40 -0500

    Created a ResourceSummary class to simplify the process of interfacing with a resource when provided an ARN.

--- common/aws/utils.py
+++ common/aws/utils.py
@@ -1,5 +1,11 @@
 from typing import Dict, Optional
 
+from policy_sentry.util.arns import parse_arn
+
+from common.config import config
+
+log = config.get_logger()
+
 
 def get_resource_tag(
     resource: Dict,
@@ -22,3 +28,63 @@ def get_resource_tag(
                 return set([] if not val else val.split(":"))
             return val
     return default
+
+
+class ResourceSummary:
+    def __init__(
+        self,
+        host: str,
+        arn: str,
+        account: str,
+        partition: str,
+        service: str,
+        region: str,
+        resource_type: str,
+        name: str,
+        resource_path: str = None,
+    ):
+        self.host = host
+        self.arn = arn
+        self.account = account
+        self.partition = partition
+        self.service = service
+        self.region = region
+        self.resource_type = resource_type
+        self.name = name
+        self.path = resource_path
+
+    @classmethod
+    async def set(cls, host: str, arn: str) -> "ResourceSummary":
+        from common.lib.aws.utils import (
+            get_bucket_location_with_fallback,
+            get_resource_account,
+        )
+
+        parsed_arn = parse_arn(arn)
+        parsed_arn["arn"] = arn
+
+        if not parsed_arn["account"]:
+            parsed_arn["account"] = await get_resource_account(arn, host)
+            if not parsed_arn["account"]:
+                raise ValueError("Resource account not found")
+
+        if parsed_arn["service"] == "s3":
+            parsed_arn["name"] = parsed_arn.pop("resource", "")
+            parsed_arn["resource_type"] = parsed_arn["service"]  # Maybe bucket?
+
+            if not parsed_arn["region"]:
+                parsed_arn["region"] = await get_bucket_location_with_fallback(
+                    parsed_arn["name"], parsed_arn["account"], host
+                )
+        else:
+            if not parsed_arn["region"]:
+                parsed_arn["region"] = config.region
+
+            if resource_path := parsed_arn.pop("resource_path", ""):
+                parsed_arn["name"] = resource_path
+                parsed_arn["resource_type"] = parsed_arn.pop("resource", "")
+            else:
+                parsed_arn["name"] = parsed_arn.pop("resource", "")
+                parsed_arn["resource_type"] = parsed_arn["service"]
+
+        return cls(host, **parsed_arn)
