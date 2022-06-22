847bedf20e9419121fb91337ae8f0e14b556c2e9
Will-NOQ <102608210+Will-NOQ@users.noreply.github.com>
2022-06-22 11:56:21 -0500

    Added loads and dumps functions to noq_json. escape_forward_slashes now False by default.

--- common/lib/noq_json/__init__.py
+++ common/lib/noq_json/__init__.py
@@ -1,1 +1,1 @@
-from common.lib.noq_json.utils import SetEncoder  # noqa: F401
+from common.lib.noq_json.utils import SetEncoder, dumps, loads  # noqa: F401
--- common/lib/noq_json/utils.py
+++ common/lib/noq_json/utils.py
@@ -2,6 +2,7 @@ import json
 from datetime import datetime
 from decimal import Decimal
 
+import ujson
 from deepdiff.model import PrettyOrderedSet
 
 
@@ -16,3 +17,29 @@ class SetEncoder(json.JSONEncoder):
         if isinstance(obj, Exception):
             return str(obj)
         return json.JSONEncoder.default(self, obj)
+
+
+def dumps(
+    obj: any,
+    ensure_ascii: bool = True,
+    double_precision: int = 9,
+    encode_html_chars: bool = False,
+    escape_forward_slashes: bool = False,
+    sort_keys: bool = False,
+    indent: int = 0,
+    **kwargs
+) -> str:
+    return ujson.dumps(
+        obj,
+        ensure_ascii,
+        double_precision,
+        encode_html_chars,
+        escape_forward_slashes,
+        sort_keys,
+        indent,
+        **kwargs
+    )
+
+
+def loads(s: str, **kwargs) -> any:
+    return ujson.loads(s, **kwargs)
