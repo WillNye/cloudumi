686f114bc029aac2e8cd5ed7318b255f57aa34e2
Matt Daue <matt@noq.dev>
2022-09-07 14:12:27 -0700

    Disable functional tests - they only work on API
    Signed-off-by: Matt Daue <matt@noq.dev>

--- common/celery_tasks/run.py
+++ common/celery_tasks/run.py
@@ -3,14 +3,15 @@ import os
 from common.celery_tasks import celery_tasks
 from common.config import config
 from common.handlers.external_processes import kill_proc, launch_proc
-from functional_tests import run_tests as functional_tests
+
+# from functional_tests import run_tests as functional_tests  ## TODO (see below)
 
 logger = config.get_logger()
 
 if os.getenv("DEBUG"):
     os.system("systemctl start ssh")
 
-functional_tests.run()
+# functional_tests.run()  ## functional tests are only for API :( - TODO: make functional tests for both API and Celery
 
 
 def run_celery_worker(log_level: str = "DEBUG", concurrency: str = "16"):
