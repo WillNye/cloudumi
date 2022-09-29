e9a28dedae52437935540da128eea906be56b765
sulaiman-mozes <sulaimanmoses13@gmail.com>
2022-09-29 19:58:22 +0000

    - fix linting

--- frontend/src/components/OnBoardingFlow/components/CreateAWSStack/CreateAWSStack.jsx
+++ frontend/src/components/OnBoardingFlow/components/CreateAWSStack/CreateAWSStack.jsx
@@ -1,10 +1,9 @@
-import React, { useCallback, useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Divider, Header, List, Message } from 'semantic-ui-react'
 import { useAuth } from 'auth/AuthProviderDefault'
 import { getCloudFormationUrl } from 'components/OnBoardingFlow/utils'
 import { useCopyToClipboard } from 'hooks/useCopyToClipboard'
 import './CreateAWSStack.scss'
-import { Bar } from 'lib/Misc'
 
 const CreateAWSStack = ({
   accountName,
