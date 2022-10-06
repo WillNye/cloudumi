6ce5b6874440ba6824a0c5425e505a38e45ea406
Austin <amcdaniel2@gmail.com>
2022-10-06 11:27:05 -0400

    moar things

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint';
+import svgrPlugin from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import checker from 'vite-plugin-checker';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [
+      svgrPlugin(),
+      tsconfigPaths(),
+      react(),
+      eslint(),
+      checker({
+        typescript: true
+      })
+    ],
+    server: {
+      port: 3000,
+      open: true,
+      host: '::',
+    }
+  };
+
+  // Make CSS module names less annoying in dev mode
+  if (command === 'serve') {
+    // @ts-ignore
+    config.css = {
+      modules: {
+        generateScopedName: '[name]-[local]'
+      }
+    };
+  }
+
+  return config;
+});
