96beca667a219f5c0084a42847f5105dfd12c51f
Steven Moy <steven@noq.dev>
2022-10-17 18:40:12 -0700

    Handle prod tenants has no master version of tenant config

--- functional_tests/test_tenants.py
+++ functional_tests/test_tenants.py
@@ -10,5 +10,9 @@ class TestTenants(FunctionalTest):
         tenant_urls = []
         for tenant in tenants:
             tenant_config = ddb.get_static_config_for_tenant_sync(tenant)
-            tenant_urls.append(tenant_config.get("url"))
+            if type(tenant_config) == dict:
+                # There seems to be an issue when a tenant does not have
+                # a master version, and get_all_tenants will include
+                # it and the subsequent get config will fail.
+                tenant_urls.append(tenant_config.get("url"))
         check_tenants(tenant_urls)
