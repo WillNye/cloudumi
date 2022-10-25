b3bfee66270a902d54701cb5c0e73ca9f0cd6143
Matt Daue <matt@noq.dev>
2022-10-25 11:42:54 -0700

    Complete rebranding of noq cli
    Signed-off-by: Matt Daue <matt@noq.dev>

--- api/handlers/v3/downloads/weep.py
+++ api/handlers/v3/downloads/weep.py
@@ -20,7 +20,7 @@ class WeepDownloadHandler(BaseHandler):
         }
 
         install_script = (
-            "mkdir -p ~/.weep ; cat <<EOF > ~/.weep/weep.yaml\n"
+            "mkdir -p ~/.noq ; cat <<EOF > ~/.noq/config.yaml\n"
             f"{yaml.dump(generated_config)}"
             "EOF\n"
         )
@@ -29,37 +29,37 @@ class WeepDownloadHandler(BaseHandler):
             {
                 "os_name": "Mac Universal Installer",
                 "download_url": (
-                    "https://public-weep-binaries.s3.us-west-2.amazonaws.com/macos_installer/"
-                    "weep-installer-macos-v0.3.24.pkg"
+                    "https://public-noq-binaries.s3.us-west-2.amazonaws.com/macos_installer/"
+                    "noq-installer-macos-v0.3.24.pkg"
                 ),
             },
             {
                 "os_name": "Mac arm64",
-                "download_url": "https://public-weep-binaries.s3.us-west-2.amazonaws.com/darwin_arm64/weep",
+                "download_url": "https://public-noq-binaries.s3.us-west-2.amazonaws.com/darwin_arm64/noq",
             },
             {
                 "os_name": "Mac x86_64",
-                "download_url": "https://public-weep-binaries.s3.us-west-2.amazonaws.com/darwin_x86_64/weep",
+                "download_url": "https://public-noq-binaries.s3.us-west-2.amazonaws.com/darwin_x86_64/noq",
             },
             {
                 "os_name": "Linux arm64",
-                "download_url": "https://public-weep-binaries.s3.us-west-2.amazonaws.com/linux_arm64/weep",
+                "download_url": "https://public-noq-binaries.s3.us-west-2.amazonaws.com/linux_arm64/noq",
             },
             {
                 "os_name": "Linux x86_64",
-                "download_url": "https://public-weep-binaries.s3.us-west-2.amazonaws.com/linux_x86_64/weep",
+                "download_url": "https://public-noq-binaries.s3.us-west-2.amazonaws.com/linux_x86_64/noq",
             },
             {
                 "os_name": "Windows arm64",
-                "download_url": "https://public-weep-binaries.s3.us-west-2.amazonaws.com/windows_arm64/weep.exe",
+                "download_url": "https://public-noq-binaries.s3.us-west-2.amazonaws.com/windows_arm64/noq.exe",
             },
             {
                 "os_name": "Windows x86_64",
-                "download_url": "https://public-weep-binaries.s3.us-west-2.amazonaws.com/windows_x86_64/weep.exe",
+                "download_url": "https://public-noq-binaries.s3.us-west-2.amazonaws.com/windows_x86_64/noq.exe",
             },
             {
                 "os_name": "Windows i386",
-                "download_url": "https://public-weep-binaries.s3.us-west-2.amazonaws.com/windows_i386/weep.exe",
+                "download_url": "https://public-noq-binaries.s3.us-west-2.amazonaws.com/windows_i386/noq.exe",
             },
         ]
 
