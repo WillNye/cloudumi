7ed74e731f60d5b30dd8c801f0f3ac9f8d2e1b5f
Steven Moy <steven@noq.dev>
2022-10-27 10:40:32 -0700

    EN-1321 Attribute Mapping for Identity Provider

--- common/lib/cognito/identity.py
+++ common/lib/cognito/identity.py
@@ -149,6 +149,32 @@ async def __get_oidc_provider(
     return oidc_provider
 
 
+def get_cognito_atrribute_mapping(
+    idp: Union[GoogleOIDCSSOIDPProvider, SamlOIDCSSOIDPProvider, OIDCSSOIDPProvider]
+):
+    # https://docs.aws.amazon.com/cognito/latest/developerguide/cognito-user-pools-specifying-attribute-mapping.html
+    provider_type = type(idp)
+    if provider_type == GoogleOIDCSSOIDPProvider:
+        return {
+            "username": "sub",
+            "email": "email",
+        }
+    elif provider_type == OIDCSSOIDPProvider:
+        return {
+            "username": "sub",
+            "email": "email",
+        }
+    elif provider_type == SamlOIDCSSOIDPProvider:
+        return (
+            {
+                "username": "NameID",
+                "email": "email",
+            },
+        )
+    else:
+        return None
+
+
 def get_tenant_user_pool_region(tenant):
     return config.get_tenant_specific_key(
         "secrets.cognito.config.user_pool_region", tenant, config.region
@@ -330,6 +356,7 @@ async def upsert_identity_provider(
                 for field in required
                 if provider_dict.get(field)
             },
+            AttributeMapping=get_cognito_atrribute_mapping(identity_provider),
         )
         await connect_idp_to_app_client(
             user_pool_id, user_pool_client_id, identity_provider, client=client
