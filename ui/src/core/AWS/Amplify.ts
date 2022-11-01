b10789381154363d37c6821900a9809622d5c319
Matt Daue <matt@noq.dev>
2022-11-01 12:03:50 -0700

    Change AWS_PROFILE to match new pattern; add comment ref to cognito
    Signed-off-by: Matt Daue <matt@noq.dev>

--- ui/src/core/AWS/Amplify.ts
+++ ui/src/core/AWS/Amplify.ts
@@ -5,6 +5,10 @@ import { Amplify } from 'aws-amplify';
  *  - https://docs.amplify.aws/lib/auth/emailpassword/q/platform/js
  *  - https://stackoverflow.com/questions/66701358/how-to-use-amazon-cognito-without-amplify
  *  - https://github.com/dbroadhurst/aws-cognito-react
+ * 
+ * Dev reference:
+ *   * cloudumi_tenant_dev-267095_noq_localhost
+ *   * arn:aws:cognito-idp:us-east-1:759357822767:userpool/us-east-1_CNoZribID
  */
 Amplify.configure({
   // Reference:
