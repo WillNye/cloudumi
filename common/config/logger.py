89dbfacd6e70bd270b00460aca8c6aeedb256ff8
Matt Daue <matt@noq.dev>
2022-11-04 12:36:21 -0700

    Fix merge
    Signed-off-by: Matt Daue <matt@noq.dev>

--- common/config/logger.py
+++ common/config/logger.py
@@ -27,6 +27,7 @@ def log_dict_func(log_level: str, account_id: str = None, role_name: str = None,
 
 def log_dict_handler(log_level: str, handler_class: object, account_id: str = None, role_name: str = None, tenant: str = None, exc: dict = {}, **kwargs: dict):
     if not log_level.upper() in ["debug", "info", "warning", "error", "critical", "exception"]:
+        log_level = "info"
     log_data = {
         "function": f"{__name__}.{handler_class.__class__.__name__}.{sys._getframe().f_code.co_name}",
         "user-agent": handler_class.request.headers.get("User-Agent"),
