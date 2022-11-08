2bded0bbbc6565b53d400b401b531a22d02c6b5e
Austin <amcdaniel2@gmail.com>
2022-11-08 11:39:30 -0500

    add example hook

--- ui/src/App/Access/AccessContainer.tsx
+++ ui/src/App/Access/AccessContainer.tsx
@@ -6,6 +6,13 @@ export const AccessContainer: FC = () => {
   // This is where the logic that interacts w/ the backend will go
   // You should avoid putting styles/etc in this component
   // I tend to only put the loader and the error state here
+
+  // Example:
+  // const { data, loading } = useQuery<AccessContainerQuery, AccessContainerQueryVariables>(ACCESS_QUERY, {});
+  // if (loading) {
+  //  return <Loader />;
+  // }
+
   const data = demoData as unknown as AccessRole[];
 
   return (
