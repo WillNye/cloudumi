0abc524fabca176ac03a237e3bbfee98ef1ad808
Austin <amcdaniel2@gmail.com>
2022-11-08 08:03:56 -0500

    add loader component

---
+++ ui/src/shared/elements/Loader/Loader.story.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import { Loader } from './Loader';
+
+export default {
+  title: 'Elements/Loader',
+  component: Loader
+};
+
+export const Basic = () => <Loader />;
