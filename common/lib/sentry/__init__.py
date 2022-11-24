37838cbb6be4e2b670d7cada218efab7a769bd3f
Curtis Castrapel <ccastrapel@gmail.com>
2022-11-24 11:29:07 -0800

    eliminate usage of SilentException, as we have Finish

--- common/lib/sentry/__init__.py
+++ common/lib/sentry/__init__.py
@@ -1,12 +1,12 @@
 from tornado.web import Finish
 
-from common.exceptions.exceptions import SilentException, TenantNoCentralRoleConfigured
+from common.exceptions.exceptions import TenantNoCentralRoleConfigured
 
 
 def before_send_event(event, hint):
     if "exc_info" in hint:
         exc_type, exc_value, tb = hint["exc_info"]
-        for exc in [TenantNoCentralRoleConfigured, SilentException, Finish]:
+        for exc in [TenantNoCentralRoleConfigured, Finish]:
             if isinstance(exc_value, exc):
                 return None
     return event
