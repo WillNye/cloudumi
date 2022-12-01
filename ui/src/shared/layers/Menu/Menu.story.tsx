78b705d5dcc178e87983fe694179572da564765a
Austin <amcdaniel2@gmail.com>
2022-12-01 11:58:54 -0500

    add list example

--- ui/src/shared/layers/Menu/Menu.story.tsx
+++ ui/src/shared/layers/Menu/Menu.story.tsx
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { Button } from 'shared/elements/Button';
+import { Divider } from 'shared/elements/Divider';
+import { List, ListItem } from 'shared/layout/List';
 import { Menu } from './Menu';
 
 export default {
@@ -30,6 +32,35 @@ export const Simple = () => {
           <li>Item2</li>
           <li>Item3</li>
         </ul>
+      </Menu>
+    </>
+  );
+};
+
+export const Lists = () => {
+  const [open, setOpen] = useState(false);
+  const buttonRef = useRef(null);
+
+  return (
+    <>
+      <Button
+        type="button"
+        ref={buttonRef}
+        onClick={() => setOpen(!open)}
+      >
+        Open
+      </Button>
+      <Menu
+        open={open}
+        onClose={() => setOpen(false)}
+        reference={buttonRef}
+      >
+        <List>
+          <ListItem>Item1</ListItem>
+          <ListItem>Item2</ListItem>
+          <Divider />
+          <ListItem>Item3</ListItem>
+        </List>
       </Menu>
     </>
   );
