208889cb552e61a3309298cd8714f148ae4770b0
Austin <amcdaniel2@gmail.com>
2022-12-01 11:44:12 -0500

    add simple list component

---
+++ ui/src/shared/layout/List/List.tsx
@@ -0,0 +1,15 @@
+import { FC, InputHTMLAttributes } from 'react';
+import classNames from 'classnames';
+import css from './List.module.css';
+
+export type ListProps = InputHTMLAttributes<HTMLDivElement>;
+
+export const List: FC<ListProps> = ({
+  className,
+  children,
+  ...rest
+}) => (
+  <div {...rest} role="list" className={classNames(className, css.list)}>
+    {children}
+  </div>
+);
