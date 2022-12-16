f9e0e6a60bf290999bb97333fdc6dcc6500aeede
Matt Daue <matt@noq.dev>
2022-12-16 13:12:27 -0800

    Update postgres vscode / globals / migrations
    Signed-off-by: Matt Daue <matt@noq.dev>

--- common/config/globals.py
+++ common/config/globals.py
@@ -1,3 +1,9 @@
+import os
+
+from sqlalchemy.ext.asyncio.engine import create_async_engine
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm.session import sessionmaker
+
 from common.config import config
 
 REDACTED_STR = "********"
@@ -11,3 +17,18 @@ class ClusterConfig:
     @property
     def dynamo_wait_time_between_retries(self) -> int:
         return config.get("_global_.dynamo.wait_time_between_retries", 5)
+
+
+ASYNC_PG_CONN_STR = f"postgresql+asyncpg://{config.get('_global_.secrets.postgres.username', os.getenv('POSTGRES_USER'))}:\
+{config.get('_global_.secrets.postgres.password', os.getenv('POSTGRES_PASS'))}@\
+{config.get('_global_.noq_db.endpoint', os.getenv('POSTGRES_ENDPOINT'))}:\
+{config.get('_global_.noq_db.port', os.getenv('POSTGRES_PORT'))}/\
+{config.get('_global_.noq_db.database', os.getenv('POSTGRES_DB'))}"
+ASYNC_PG_ENGINE = create_async_engine(
+    ASYNC_PG_CONN_STR, connect_args={"server_settings": {"jit": "off"}}
+)
+ASYNC_PG_SESSION = sessionmaker(
+    ASYNC_PG_ENGINE,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
