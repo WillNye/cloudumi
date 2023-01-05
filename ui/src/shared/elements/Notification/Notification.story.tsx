2418c37cb10cf305a84f8550de41d9eb74080c4a
sulaiman-mozes <sulaimanmoses13@gmail.com>
2023-01-06 00:39:55 +0300

    - complete notification component

--- ui/src/shared/elements/Notification/Notification.story.tsx
+++ ui/src/shared/elements/Notification/Notification.story.tsx
@@ -1,4 +1,5 @@
-import { Notification } from './Notification';
+import { Button } from '../Button';
+import { Notification, NotificationType } from './Notification';
 
 export default {
   title: 'Elements/Notification',
@@ -8,19 +9,54 @@ export default {
 export const Basic = () => {
   return (
     <>
-      <Notification message="This is a left tooltip">
-        <div>Left</div>
+      <Notification
+        header="Are you sure you want to delete this request?"
+        message="This action cannot be reversed."
+      />
+      <br />
+      <Notification
+        type={NotificationType.INFO}
+        header="New version available"
+      />
+      <br />
+      <Notification
+        type={NotificationType.INFO}
+        header="New version available"
+        message="An improved version of NOQ is available now, please refresh to update."
+      >
+        <br />
+        <Button size="small">Refresh Now</Button>
       </Notification>
       <br />
-      <Notification message="This is a right tooltip">
-        <div>Right</div>
-      </Notification>
+      <Notification
+        type={NotificationType.SUCCESS}
+        header="Request submitted"
+      />
       <br />
-      <Notification message="This is a bottom tooltip">
-        <div>Bottom</div>
-      </Notification>
+      <Notification
+        type={NotificationType.SUCCESS}
+        header="Request submitted"
+        message="Your access request is submitted successfully."
+      />
       <br />
-      <Notification message="This is a top tooltip">Top</Notification>
+      <Notification type={NotificationType.ERROR} header="Access declined" />
+      <br />
+      <Notification
+        type={NotificationType.ERROR}
+        header="Access declined"
+        message="Your access request is declined."
+      />
+      <br />
+      <Notification
+        type={NotificationType.WARNING}
+        header="Request not submitted"
+      />
+      <br />
+      <Notification
+        type={NotificationType.WARNING}
+        header="Request not submitted"
+        message="Your access request is not submitted."
+      />
     </>
   );
 };
--- ui/src/shared/elements/Notification/Notification.tsx
+++ ui/src/shared/elements/Notification/Notification.tsx
@@ -1,33 +1,47 @@
-import classNames from 'classnames';
-import { FC, ReactNode, useMemo } from 'react';
-import { Button } from '../Button';
+import { FC, ReactNode } from 'react';
 
 import styles from './Notification.module.css';
 import { Icon } from '../Icon';
 
+export enum NotificationType {
+  ERROR = 'error',
+  SUCCESS = 'success',
+  INFO = 'info',
+  WARNING = 'warning'
+}
+
 interface NotificationProps {
-  type?: 'error' | 'success' | 'info' | 'warning';
-  message: string;
-  children: ReactNode;
+  type?: NotificationType;
+  message?: string;
+  header: string;
+  children?: ReactNode;
+  onClose?: () => void;
+  showCloseIcon?: boolean;
 }
 
 export const Notification: FC<NotificationProps> = ({
   type,
   message,
-  children
+  header,
+  children,
+  showCloseIcon = true,
+  onClose
 }) => {
-  const classes = useMemo(() => {
-    return classNames(styles.notification, {});
-  }, []);
-
   return (
-    <div className={classes}>
-      <div>
-        <Icon name="notification-info" color="primary" />
-        <p>{message}</p>
-        <Button icon="close" color="secondary" variant="outline"></Button>
+    <div className={styles.notification}>
+      {type && (
+        <Icon name={`notification-${type}`} color="primary" size="large" />
+      )}
+      <div className={styles.content}>
+        <div className={styles.header}>{header}</div>
+        {message && <p className={styles.text}>{message}</p>}
+        {children}
       </div>
-      {children}
+      {showCloseIcon && (
+        <div className={styles.close} onClick={onClose}>
+          <Icon name="close" color="secondary" size="large" />
+        </div>
+      )}
     </div>
   );
 };
