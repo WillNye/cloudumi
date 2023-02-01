2e1d0cef217196e0becbd76d70638daf02dd2af0
Curtis Castrapel <curtis@noq.dev>
2023-02-01 15:37:08 -0800

    WIP

---
+++ common/tenants/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, Integer, String, and_
+from sqlalchemy.sql import select
+
+from common.config.globals import ASYNC_PG_SESSION
+from common.pg_core.models import Base, SoftDeleteMixin
+
+
+class Tenant(SoftDeleteMixin, Base):
+    __tablename__ = "tenant"
+
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    name = Column(String, index=True)
+    organization_id = Column(String)
+    
+    @classmethod
+    async def get_by_id(cls, tenant_id):
+        async with ASYNC_PG_SESSION() as session:
+            async with session.begin():
+                stmt = select(Tenant).where(
+                    and_(
+                        Tenant.id == tenant_id,
+                        Tenant.deleted == False,  # noqa
+                    )
+                )
+                tenant = await session.execute(stmt)
+                return tenant.scalars().first()
+
+    @classmethod
+    async def get_by_name(cls, tenant_name):
+        async with ASYNC_PG_SESSION() as session:
+            async with session.begin():
+                stmt = select(Tenant).where(
+                    and_(
+                        Tenant.name == tenant_name,
+                        Tenant.deleted == False,  # noqa
+                    )
+                )
+                tenant = await session.execute(stmt)
+                return tenant.scalars().first()
