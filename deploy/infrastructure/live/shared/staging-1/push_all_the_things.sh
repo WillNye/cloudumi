aac892ba49c790225b70851fdd0715ac5a9e412c
Curtis <ccastrapel@gmail.com>
2023-04-09 09:56:45 -0400

    Locust automatically runs during deploy

--- deploy/infrastructure/live/shared/staging-1/push_all_the_things.sh
+++ deploy/infrastructure/live/shared/staging-1/push_all_the_things.sh
@@ -116,3 +116,4 @@ echo
 echo "Deploying Service - $VERSION"
 echo
 python deploy/infrastructure/live/shared/staging-1/ecs_deployer.py
+python -m locust -f load_tests/locustfile.py --headless -u 10 -r 5 -t 60
--- load_tests/locustfile.py
+++ load_tests/locustfile.py
@@ -24,7 +24,7 @@ TEST_USER_DOMAIN_US = (
 
 
 class LoadTest(FastHttpUser):
-    host = f"{TEST_USER_DOMAIN}"
+    host = "http://localhost:8092"
     default_headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
     }
@@ -38,43 +38,45 @@ class LoadTest(FastHttpUser):
         )
     )
 
-    @task
-    def login(self):
-        with self.client.request(
-            "GET",
-            f"{TEST_USER_DOMAIN}/login",
-            headers={
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "en-US,en;q=0.9",
-                "Cache-Control": "max-age=0",
-                "Connection": "keep-alive",
-                "Host": "cloudumidev.com:3000",
-                "Sec-Fetch-Dest": "document",
-                "Sec-Fetch-Mode": "navigate",
-                "Sec-Fetch-Site": "none",
-                "Sec-Fetch-User": "?1",
-                "Upgrade-Insecure-Requests": "1",
-                "sec-ch-ua": '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": '"macOS"',
-            },
-            catch_response=True,
-        ) as resp:
-            assert resp.status_code == 200
+    # # TODO: Uncomment this function when we switch to new UI
+    # @task
+    # def login(self):
+    #     with self.client.request(
+    #         "GET",
+    #         f"/login",
+    #         headers={
+    #             "Host": TEST_USER_DOMAIN,
+    #             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    #             "Accept-Encoding": "gzip, deflate, br",
+    #             "Accept-Language": "en-US,en;q=0.9",
+    #             "Cache-Control": "max-age=0",
+    #             "Connection": "keep-alive",
+    #             "Sec-Fetch-Dest": "document",
+    #             "Sec-Fetch-Mode": "navigate",
+    #             "Sec-Fetch-Site": "none",
+    #             "Sec-Fetch-User": "?1",
+    #             "Upgrade-Insecure-Requests": "1",
+    #             "sec-ch-ua": '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+    #             "sec-ch-ua-mobile": "?0",
+    #             "sec-ch-ua-platform": '"macOS"',
+    #             "X-Forwarded-For": "127.0.0.1"
+    #         },
+    #         catch_response=True,
+    #     ) as resp:
+    #         assert resp.status_code == 200
 
     @task
     def user_profile_auth(self):
         with self.rest(
             "GET",
-            f"{TEST_USER_DOMAIN}/api/v2/user_profile",
+            "/api/v2/user_profile",
             headers={
+                "Host": TEST_USER_DOMAIN,
                 "Accept": "application/json, text/plain, */*",
                 "Accept-Encoding": "gzip, deflate, br",
                 "Accept-Language": "en-US,en;q=0.9",
                 "Connection": "keep-alive",
                 "Cookie": f"noq_auth={self.token}",
-                "Host": "cloudumidev.com:3000",
                 "Referer": f"{TEST_USER_DOMAIN}/login",
                 "Sec-Fetch-Dest": "empty",
                 "Sec-Fetch-Mode": "cors",
@@ -82,6 +84,7 @@ class LoadTest(FastHttpUser):
                 "sec-ch-ua": '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
                 "sec-ch-ua-mobile": "?0",
                 "sec-ch-ua-platform": '"macOS"',
+                "X-Forwarded-For": "127.0.0.1",
             },
         ) as resp:
             assert resp.status_code == 200
