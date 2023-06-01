919d98463833e1adb34fdecbffaf36874c90503e
Curtis Castrapel <curtis@noq.dev>
2023-05-31 17:02:12 -0700

    Add Downloads Page

---
+++ ui/src/App/Downloads/Downloads.tsx
@@ -0,0 +1,134 @@
+import React, { FC, useEffect, useState, useMemo } from 'react';
+import axios from 'axios';
+import { Table } from 'shared/elements/Table';
+import { Segment } from 'shared/layout/Segment';
+import { CopyBlock, dracula } from 'react-code-blocks';
+import css from './Downloads.module.css';
+
+interface DownloadLink {
+  download_url: string;
+  os_name: string;
+}
+
+interface DownloadResponse {
+  install_script: string;
+  install_script_windows: string;
+  download_links: DownloadLink[];
+}
+
+enum DOWNLOAD_TABS {
+  LINUX_MAC = 'Linux/Mac',
+  WINDOWS = 'Windows'
+}
+
+const Downloads: FC = () => {
+  const [noqInstallScript, setNoqInstallScript] = useState<string>('');
+  const [noqInstallScriptWindows, setNoqInstallScriptWindows] =
+    useState<string>('');
+  const [noqDownloadTable, setNoqDownloadTable] = useState<DownloadLink[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get<DownloadResponse>(
+        '/api/v3/downloads/noq'
+      );
+      const resJson = response.data;
+
+      setNoqInstallScript(resJson.install_script);
+      setNoqInstallScriptWindows(resJson.install_script_windows);
+      setNoqDownloadTable(resJson.download_links);
+    };
+    fetchData();
+  }, []);
+
+  const isWindows = navigator.userAgent.toLowerCase().indexOf('win') > -1;
+  const [currentTab, setCurrentTab] = useState<DOWNLOAD_TABS>(
+    isWindows ? DOWNLOAD_TABS.WINDOWS : DOWNLOAD_TABS.LINUX_MAC
+  );
+
+  const content = useMemo(() => {
+    if (currentTab === DOWNLOAD_TABS.WINDOWS) {
+      return (
+        noqInstallScriptWindows && (
+          <div className={css.codeBlockContainer}>
+            <CopyBlock
+              text={noqInstallScriptWindows}
+              language={'powershell'}
+              showLineNumbers={false}
+              theme={dracula}
+            />
+          </div>
+        )
+      );
+    }
+
+    return (
+      noqInstallScript && (
+        <div className={css.codeBlockContainer}>
+          <CopyBlock
+            text={noqInstallScript}
+            language={'shell'}
+            showLineNumbers={false}
+            theme={dracula}
+          />
+        </div>
+      )
+    );
+  }, [currentTab, noqInstallScript, noqInstallScriptWindows]);
+
+  return (
+    <Segment>
+      <h1>Downloads</h1>
+      <p>
+        The NOQ CLI tool makes it easy to retrieve and use AWS credentials
+        securely, when paired with the NOQ Cloud platform. Download `noq` CLI
+        for your operating system below, then run the following command to
+        configure it:
+      </p>
+      <div className={css.nav}>
+        <ul className={css.navList}>
+          <li
+            className={`${css.navItem} ${
+              currentTab === DOWNLOAD_TABS.LINUX_MAC && css.isActive
+            }`}
+            onClick={() => setCurrentTab(DOWNLOAD_TABS.LINUX_MAC)}
+          >
+            Linux/Mac
+          </li>
+          <li
+            className={`${css.navItem} ${
+              currentTab === DOWNLOAD_TABS.WINDOWS && css.isActive
+            }`}
+            onClick={() => setCurrentTab(DOWNLOAD_TABS.WINDOWS)}
+          >
+            Windows
+          </li>
+        </ul>
+      </div>
+      {content}
+      <br />
+      {noqDownloadTable.length ? (
+        <Table
+          columns={[
+            {
+              Header: 'OS Name',
+              accessor: 'os_name',
+              Cell: ({ cell: { value }, row: { original } }) => (
+                <a
+                  href={original.download_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {value}
+                </a>
+              )
+            }
+          ]}
+          data={noqDownloadTable}
+        />
+      ) : null}
+    </Segment>
+  );
+};
+
+export default Downloads;
