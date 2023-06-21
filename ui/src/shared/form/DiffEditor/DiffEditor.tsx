fd2c8421d63e2072e1f7ba59f8e91bbcd49a3d78
Curtis Castrapel <curtis@noq.dev>
2023-06-21 10:26:39 -0700

    Monaco Typeahead for actions and resources

--- ui/src/shared/form/DiffEditor/DiffEditor.tsx
+++ ui/src/shared/form/DiffEditor/DiffEditor.tsx
@@ -1,13 +1,21 @@
-import React, { FC, useState } from 'react';
-import { DiffEditor as MonacoDiffEditor } from '@monaco-editor/react';
+import React, { FC, useEffect, useState } from 'react';
+import {
+  DiffEditor as MonacoDiffEditor,
+  useMonaco
+} from '@monaco-editor/react';
 import './DiffEditor.module.css';
 import { Button } from 'shared/elements/Button'; // Update the path according to your Button component
 import styles from './DiffEditor.module.css';
+import {
+  getMonacoCompletions,
+  getMonacoTriggerCharacters
+} from 'core/utils/monacoUtils';
 
 interface DiffEditorProps {
   original: string;
   modified: string;
   language?: string;
+  onChange?: (value: string) => void;
 }
 
 const DEFAULT_EDITOR_OPTIONS = {
@@ -26,9 +34,11 @@ const DEFAULT_EDITOR_OPTIONS = {
 export const DiffEditor: FC<DiffEditorProps> = ({
   original,
   modified,
-  language = 'yaml'
+  language = 'yaml',
+  onChange
 }) => {
   const [renderSideBySide, setRenderSideBySide] = useState(true);
+  const monaco = useMonaco();
 
   const options = {
     ...DEFAULT_EDITOR_OPTIONS,
@@ -37,6 +47,25 @@ export const DiffEditor: FC<DiffEditorProps> = ({
 
   const handleSideBySideToggle = () => {
     setRenderSideBySide(!renderSideBySide);
+  };
+
+  useEffect(() => {
+    if (!monaco) {
+      return;
+    }
+    monaco.languages.registerCompletionItemProvider(language, {
+      triggerCharacters: getMonacoTriggerCharacters(),
+      async provideCompletionItems(model, position) {
+        return await getMonacoCompletions(model, position, monaco);
+      }
+    });
+  }, [monaco]);
+
+  const editorDidMount = (editor, monaco) => {
+    const modifiedEditor = editor.getModifiedEditor();
+    modifiedEditor.onDidChangeModelContent(_ => {
+      onChange && onChange(modifiedEditor.getValue());
+    });
   };
 
   return (
@@ -55,6 +84,7 @@ export const DiffEditor: FC<DiffEditorProps> = ({
         modified={modified}
         options={options}
         theme="vs-dark"
+        onMount={editorDidMount}
       />
     </div>
   );
