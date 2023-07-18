e855ee0bfa725bed5462cdaec8d74a4213a41277
Jonathan Loscalzo <loscalzo.jony@gmail.com>
2023-07-18 12:47:46 -0300

    add cache_iambic_data_for_tenant on SSO User first login

--- common/lib/auth/user_management.py
+++ common/lib/auth/user_management.py
@@ -5,11 +5,27 @@ from common.users.models import User
 
 
 async def maybe_create_users_groups_in_database(
-    db_tenant: Tenant, user: str, groups: list[str], description: str, managed_by: str
+    db_tenant: Tenant | str,
+    user: str,
+    groups: list[str],
+    description: str,
+    managed_by: str,
 ):
     db_user = await User.get_by_email(db_tenant, user)
     new_groups = []
     if not db_user:
+        if managed_by == "SSO":
+            from common.celery_tasks.celery_tasks import app as celery_app
+
+            celery_app.send_task(
+                "common.celery_tasks.celery_tasks.cache_iambic_data_for_tenant",
+                kwargs={
+                    "tenant": db_tenant
+                    if isinstance(db_tenant, str)
+                    else db_tenant.name
+                },
+            )
+
         db_user = await User.create(
             db_tenant,
             user,
