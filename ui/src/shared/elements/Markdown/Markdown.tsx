86c2aa3ed6aa6079edffe76874a0ff5585be14f7
Jonathan Loscalzo <loscalzo.jony@gmail.com>
2023-07-21 11:12:23 -0300

    add styles for blockquote, table and code

--- ui/src/shared/elements/Markdown/Markdown.tsx
+++ ui/src/shared/elements/Markdown/Markdown.tsx
@@ -3,6 +3,7 @@ import remarkBreaks from 'remark-breaks';
 import ReactMarkdown from 'react-markdown';
 import remarkGithub from 'remark-github';
 import rehypeRaw from 'rehype-raw';
+import styles from './Markdown.module.css';
 
 export const NoqMarkdown = ({ markdown }) => {
   return (
@@ -17,6 +18,17 @@ export const NoqMarkdown = ({ markdown }) => {
       ]}
       rehypePlugins={[rehypeRaw]}
       remarkRehypeOptions={{ allowDangerousHtml: true }}
+      components={{
+        blockquote: ({ node, ...props }) => (
+          <blockquote className={styles.blockquote} {...props}></blockquote>
+        ),
+        table: ({ node, ...props }) => (
+          <table className={styles.table} {...props}></table>
+        ),
+        code: ({ node, ...props }) => (
+          <code className={styles.code} {...props}></code>
+        )
+      }}
     >
       {markdown}
     </ReactMarkdown>
