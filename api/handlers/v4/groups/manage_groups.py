7ffcaea5d71418b46f7925318f8d637256acc9b3
Jonathan Loscalzo <loscalzo.jony@gmail.com>
2023-07-26 10:12:10 -0300

    fix: rearrange typing and  add comments on email getter

--- api/handlers/v4/groups/manage_groups.py
+++ api/handlers/v4/groups/manage_groups.py
@@ -6,7 +6,7 @@ from email_validator import validate_email
 from common.groups.models import Group
 from common.handlers.base import BaseAdminHandler
 from common.lib.filter import PaginatedQueryResponse, filter_data_with_sqlalchemy
-from common.models import WebResponse
+from common.models import Status2, WebResponse
 
 
 class ManageListGroupsHandler(BaseAdminHandler):
@@ -24,9 +24,11 @@ class ManageListGroupsHandler(BaseAdminHandler):
             errors = [str(exc)]
             self.write(
                 WebResponse(
+                    status=Status2.error,
                     errors=errors,
                     status_code=500,
                     count=len(errors),
+                    reason="GenericException",
                 ).dict(exclude_unset=True, exclude_none=True)
             )
             # reason is in the response header and cannot contain newline
@@ -38,7 +40,8 @@ class ManageListGroupsHandler(BaseAdminHandler):
 
         self.write(
             WebResponse(
-                success="success",
+                status=Status2.success,
+                reason=None,
                 status_code=200,
                 data=query_response.dict(exclude_unset=True, exclude_none=True),
             ).dict(exclude_unset=True, exclude_none=True)
@@ -60,7 +63,8 @@ class ManageGroupsHandler(BaseAdminHandler):
             )
         self.write(
             WebResponse(
-                success="success",
+                status=Status2.success,
+                reason=None,
                 status_code=200,
                 data={"groups": groups},
             ).dict(exclude_unset=True, exclude_none=True)
@@ -70,13 +74,16 @@ class ManageGroupsHandler(BaseAdminHandler):
         data = tornado.escape.json_decode(self.request.body)
         group_name = data.get("name")
         group_description = data.get("description")
+        # at the moment, we don't allow changing the email
         group_email = data.get("email")
+
         if group_email and not validate_email(group_email):
             self.set_status(403)
             self.write(
                 WebResponse(
-                    status="error",
+                    status=Status2.error,
                     status_code=403,
+                    reason="Invalid email address",
                     data={"message": "Invalid email address"},
                 ).dict(exclude_unset=True, exclude_none=True)
             )
@@ -87,8 +94,9 @@ class ManageGroupsHandler(BaseAdminHandler):
             self.set_status(400)
             self.write(
                 WebResponse(
-                    status="error",
+                    status=Status2.error,
                     status_code=400,
+                    reason="Group name already taken",
                     data={"message": "Group name already taken"},
                 ).dict(exclude_unset=True, exclude_none=True)
             )
@@ -104,9 +112,10 @@ class ManageGroupsHandler(BaseAdminHandler):
 
         self.write(
             WebResponse(
-                success="success",
+                status=Status2.success,
                 message="Successfully created group",
                 status_code=200,
+                reason=None,
                 data={
                     "id": created_group.id,
                     "name": created_group.name,
@@ -122,7 +131,8 @@ class ManageGroupsHandler(BaseAdminHandler):
         group_id = data.get("id")
         group_name = data.get("name")
         group_description = data.get("description")
-        group_email = data.get("name")
+        # at the moment, we don't allow changing the email
+        group_email = data.get("email")
 
         db_group = await Group.get_by_id(self.ctx.db_tenant, group_id)
 
@@ -130,8 +140,9 @@ class ManageGroupsHandler(BaseAdminHandler):
             self.set_status(400)
             self.write(
                 WebResponse(
-                    status="error",
+                    status=Status2.error,
                     status_code=400,
+                    reason="Group does not exist",
                     data={"message": "Group does not exist"},
                 ).dict(exclude_unset=True, exclude_none=True)
             )
@@ -145,7 +156,10 @@ class ManageGroupsHandler(BaseAdminHandler):
         )
         self.write(
             WebResponse(
-                success="success", status_code=200, data=new_db_group.dict()
+                status=Status2.success,
+                reason=None,
+                status_code=200,
+                data=new_db_group.dict(),
             ).dict(exclude_unset=True, exclude_none=True)
         )
 
@@ -158,8 +172,9 @@ class ManageGroupsHandler(BaseAdminHandler):
             self.set_status(400)
             self.write(
                 WebResponse(
-                    status="error",
+                    status=Status2.error,
                     status_code=400,
+                    reason="Group does not exist",
                     data={"message": "Group does not exist"},
                 ).dict(exclude_unset=True, exclude_none=True)
             )
@@ -168,8 +183,9 @@ class ManageGroupsHandler(BaseAdminHandler):
         await db_group.delete()
         self.write(
             WebResponse(
-                success="success",
+                status=Status2.success,
                 status_code=200,
+                reason="Group deleted successfully",
                 data={"message": "Group deleted successfully"},
             ).dict(exclude_unset=True, exclude_none=True)
         )
